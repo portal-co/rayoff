5b48bafb688dfb90b0c976255fea4b507b82944e
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-02-04 13:16:14 -0500

    switch to framing

--- src/lib.rs
+++ src/lib.rs
@@ -5,118 +5,57 @@ pub use rayon;
 
 #[cfg(feature = "rayon")]
 #[macro_export]
-macro_rules! map {
-    ($a:expr => |$p:pat_param|$b:expr) => {
+macro_rules! frames {
+    ($a:expr => $($f:ident $(:|$p:pat_param|$b:expr)?),*) => {
         if false {
             $crate::core::iter::IntoIterator::into_iter($a)
-                .map(|$p| $b)
-                .collect()
+                $(.$f($(|$p| $b)?))*
         } else {
             $crate::rayon::iter::IntoParallelIterator::into_par_iter($a)
-                .map(|$p| $b)
-                .collect()
+                $(.$f($(|$p| $b)?))*
         }
     };
 }
 #[cfg(not(feature = "rayon"))]
 #[macro_export]
-macro_rules! map {
-    ($a:expr => |$p:pat_param|$b:expr) => {
+macro_rules! frames {
+    ($a:expr => $($f:ident $(:|$p:pat_param|$b:expr)?),*) => {
         $crate::core::iter::IntoIterator::into_iter($a)
-            .map(|$p| $b)
-            .collect()
+            $(.$f($(|$p| $b)?))*
     };
 }
 
-#[cfg(feature = "rayon")]
+#[macro_export]
+macro_rules! map {
+    ($a:expr => |$p:pat_param|$b:expr) => {
+        $crate::frames!($a => map:|$p| $b, collect)
+    };
+}
+
 #[macro_export]
 macro_rules! sum {
     ($a:expr => |$p:pat_param|$b:expr) => {
-        if false {
-            $crate::core::iter::IntoIterator::into_iter($a).map(|$p| $b).sum()
-        } else {
-            $crate::rayon::iter::IntoParallelIterator::into_par_iter($a)
-                .map(|$p| $b)
-                .sum()
-        }
-    };
-}
-#[cfg(not(feature = "rayon"))]
-#[macro_export]
-macro_rules! sum {
-    ($a:expr => |$p:pat_param|$b:expr) => {
-        $crate::core::iter::IntoIterator::into_iter($a).map(|$p| $b).sum()
+        $crate::frames!($a => map:|$p| $b, sum)
     };
 }
 
-#[cfg(feature = "rayon")]
 #[macro_export]
 macro_rules! any {
     ($a:expr => |$p:pat_param|$b:expr) => {
-        if false {
-            $crate::core::iter::IntoIterator::into_iter($a).any(|$p| $b)
-        } else {
-            $crate::rayon::iter::IntoParallelIterator::into_par_iter($a).any(|$p| $b)
-        }
-    };
-}
-#[cfg(not(feature = "rayon"))]
-#[macro_export]
-macro_rules! any {
-    ($a:expr => |$p:pat_param|$b:expr) => {
-        $crate::core::iter::IntoIterator::into_iter($a).any(|$p| $b)
+        $crate::frames!($a => any:|$p| $b)
     };
 }
 
-#[cfg(feature = "rayon")]
 #[macro_export]
 macro_rules! flat_map {
     ($a:expr => |$p:pat_param|$b:expr) => {
-        if false {
-            $crate::core::iter::IntoIterator::into_iter($a)
-                .flat_map(|$p| $b)
-                .collect()
-        } else {
-            $crate::rayon::iter::IntoParallelIterator::into_par_iter($a)
-                .flat_map(|$p| $b)
-                .collect()
-        }
-    };
-}
-#[cfg(not(feature = "rayon"))]
-#[macro_export]
-macro_rules! flat_map {
-    ($a:expr => |$p:pat_param|$b:expr) => {
-        $crate::core::iter::IntoIterator::into_iter($a)
-            .flat_map(|$p| $b)
-            .collect()
+        $crate::frames!($a => flat_map:|$p| $b, collect)
     };
 }
 
-#[cfg(feature = "rayon")]
 #[macro_export]
 macro_rules! enumerate_map {
     ($a:expr => |$p:pat_param|$b:expr) => {
-        if false {
-            $crate::core::iter::IntoIterator::into_iter($a)
-                .enumerate()
-                .map(|$p| $b)
-                .collect()
-        } else {
-            $crate::rayon::iter::IntoParallelIterator::into_par_iter($a)
-                .enumerate()
-                .map(|$p| $b)
-                .collect()
-        }
-    };
-}
-#[cfg(not(feature = "rayon"))]
-#[macro_export]
-macro_rules! enumerate_map {
-    ($a:expr => |$p:pat_param|$b:expr) => {
-        $crate::core::iter::IntoIterator::into_iter($a)
-            .enumerate()
-            .map(|$p| $b)
-            .collect()
+        $crate::frames!($a => enumerate, map:|$p| $b, collect)
     };
 }
